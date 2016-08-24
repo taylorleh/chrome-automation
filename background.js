b33a00a720c919cc59234d9bb2d4a8b9a0e1dc87
Taylor Lehman <taylorlehman111@gmail.com>
2016-08-24 00:48:16 -0700

    moves popup dynamic content messaging from background

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ var recording = false;
 var playing = false;
 
 var LLDecorator = {
-  attachToTail: function (frag){
+  attachToTail: function(frag){
     this._asArray = this._asArray.concat(frag._asArray);
     var newTail = frag.tail;
     this.size += frag.size;
@@ -19,22 +19,21 @@ var LLDecorator = {
 
   },
 
-  saveFragment: function (frag){
+  saveFragment: function(frag){
     var count = Object.keys(this.pageFragments).length + 1;
     this.pageFragments[count] = frag._asArray;
   }
 };
 
-var RecordingsModule = (function (){
+var RecordingsModule = (function(){
   var pages = {};              // hash of urls and LL recordings
   var currentRecording = null; // linked List of current (in progress) recording
   var initialPage = null; // url of starting page of recording so can append after
   var ongoingFragIndex = 0; // for multi page automations - keeps track of which LL fragment to send on page load
   var currentPlayback = null; // set after ui starts playback on first page.
 
-  
   return {
-    append: function (fragment){
+    append: function(fragment){
       if (currentRecording) { // append to current automation fragment
         LLDecorator.saveFragment.call(currentRecording, fragment.cache);
         LLDecorator.attachToTail.call(currentRecording, fragment.cache);
@@ -45,24 +44,24 @@ var RecordingsModule = (function (){
       }
     },
 
-    begin: function (url){
+    begin: function(url){
       if (!initialPage && !currentRecording) {
         initialPage = url;
       } else {
       }
     },
 
-    showBuilt: function (){
+    showBuilt: function(){
     },
 
-    build: function (){
+    build: function(){
       currentRecording.totalFragments = Object.keys(currentRecording.pageFragments).length;
       pages[initialPage] = currentRecording;
       currentRecording = null;
       initialPage = null;
     },
 
-    getOngoingFragment: function (url){
+    getOngoingFragment: function(url){
       var pageAutomations;
 
       if (url && !playing) { // initial playback, return first fragment
@@ -89,37 +88,21 @@ var RecordingsModule = (function (){
   };
 }());
 
-
-var MESSAGE_FLOWS = {
-  'RECORD': {
-    'ACCEPT': 'Recording Started!',
-    'IN_PROGRESS': 'Recording already in progress. Please start a new recording',
-    'DEFAULT': 'Click record to start a new recording'
-  },
-  'PLAYBACK': {
-    'RUNNING': 'Playback Started',
-    'IN_PROGRESS': 'Playback already in progress'
-  }
-};
-
-var INBOX = {
+const INBOX = {
   EXTENSION: { // messages received from the extension (popup.js) invoked by user interaction
-    STATE_REQUEST: function (request, sender, sendResponse){
+    STATE_REQUEST: function(request, sender, sendResponse){
       if (recording && !playing) {
-        sendResponse({
-          value: MESSAGE_FLOWS.RECORD.ACCEPT
-        })
+        sendResponse({value: 'ACCEPT'});
       } else if (playing && !recording) {
+        console.log('background:: nothing to send popup for - playing, !recording');
       } else if (!recording && !playing) {
-        sendResponse({
-          value: MESSAGE_FLOWS.RECORD.DEFAULT
-        })
+        sendResponse({value: 'DEFAULT'})
       } else {
       }
     },
 
-    STOP: function (request, sender, sendResponse){
-      messageContentScript({message: 'STOP'}, function (csresp){
+    STOP: function(request, sender, sendResponse){
+      messageContentScript({message: 'STOP'}, function(csresp){
         recording = false;
         playing = false;
         if (csresp.details) { // check if there were any actions on the page where 'stop' was clicked
@@ -128,26 +111,25 @@ var INBOX = {
         RecordingsModule.showBuilt();
         RecordingsModule.build();
         sendResponse({
-          value: MESSAGE_FLOWS.RECORD.DEFAULT
+          value: 'DEFAULT'
         })
 
       });
     },
 
-    PLAYBACK: function (request, sender, sendResponse){
+    PLAYBACK: function(request, sender, sendResponse){
       if (playing || recording) {
         sendResponse({
-          value: MESSAGE_FLOWS.PLAYBACK.IN_PROGRESS
+          value: 'IN_PROGRESS'
         });
         return;
       } else {
         sendResponse({
-          message: MESSAGE_FLOWS.PLAYBACK.RUNNING
+          value: 'RUNNING'
         });
       }
 
-
-      getPageUrl(function (tab){
+      getPageUrl(function(tab){
         var frag = RecordingsModule.getOngoingFragment(tab.url);
         if (!frag) {
         }
@@ -158,19 +140,20 @@ var INBOX = {
       })
     },
 
-    RECORD: function (request, sender, sendResponse){
-      var responseHandler = csResponse(request);
+    RECORD: function(request, sender, sendResponse){
       recording = true;
-      messageContentScript({message: 'RECORD'}, function (response, reply){
-        reply && (responseQ.push(sendResponse)) && reply(response);
-
+      messageContentScript({message: 'RECORD'}, function(response){
         if (response.message === 'ACCEPT') { // if content script accepts
           RecordingsModule.begin(response.location);
+          sendResponse({
+            value: 'ACCEPT'
+          });
 
         } else { // else if content script denies
-
+          sendResponse({
+            value: 'IN_PROGRESS'
+          });
         }
-
 
       });
       return true;
@@ -178,9 +161,9 @@ var INBOX = {
     }
   },
   CONTENT_SCRIPT: {
-    STATE_REQUEST: function (request, sender, sendResponse){
+    STATE_REQUEST: function(request, sender, sendResponse){
 
-      var state = 'DEFAULT';
+      let state = 'DEFAULT';
       if (recording && !playing) {
         state = 'RECORDING';
       } else if (playing && !recording) {
@@ -193,63 +176,40 @@ var INBOX = {
       sendResponse({
         value: state,
         details: frag || null
-      }, function (){
+      }, function(){
       });
     },
-    UNLOAD: function (request, sender, sendResponse){
+
+    UNLOAD: function(request, sender, sendResponse){
       // pages[request.location.href] = request.details;
       RecordingsModule.append(request.details);
     }
   }
 };
 
+const messageContentScript = function(msg, cb){
 
-var csResponse = function (outbox){
-  var action = outbox.message
-    , validResponses = MESSAGE_FLOWS[action];
-
-  return function (inbox){
-    var messageToPopup;
-
-    try {
-      messageToPopup = validResponses[inbox.message];
-    } catch (e) {
-      messageToPopup = 'ERROR: Did not receive response from script!';
-    }
-
-    responseQ.shift()({
-      value: messageToPopup,
-      classes: 'green'
-    });
-  }
-};
-
-
-// to send message to content script, we must first query the tab
-var messageContentScript = function (msg, cb){
-
-  var responseHandler = csResponse(msg);
-
-  chrome.tabs.query({active: true}, function (tabs){
-    chrome.tabs.sendMessage(tabs[0].id, msg, function (response){
-      if (!cb) {
-        responseHandler(response);
-      } else if (cb) {
-        cb(response, responseHandler);
+  chrome.tabs.query({active: true}, function(tabs){
+    chrome.tabs.sendMessage(tabs[0].id, msg, function(response){
+      if (cb) {
+        cb(response)
       }
     });
-
   });
 };
 
-var getPageUrl = function (cb){
-  chrome.tabs.getSelected(function (tab){
+/**
+ * @function getPageUrl
+ * @description to send message to content script, we must first query the tab
+ * @param cb
+ */
+const getPageUrl = function(cb){
+  chrome.tabs.getSelected(function(tab){
     cb(tab);
   })
 };
 
-
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse){
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
 
   var from = sender.tab ? 'CONTENT_SCRIPT' : 'EXTENSION';
   var narrowedResponse = INBOX[from][request.message];
--- popup.js
+++ popup.js
@@ -1,62 +1,92 @@
-const UI = function (){
+const MESSAGE_FLOWS = {
+  'RECORD': {
+    'ACCEPT': 'Recording Started!',
+    'IN_PROGRESS': 'Recording already in progress. Please start a new recording',
+    'DEFAULT': 'Click record to start a new recording'
+  },
+  'PLAYBACK': {
+    'RUNNING': 'Playback Started',
+    'IN_PROGRESS': 'Playback already in progress'
+  }
+};
+
+const UI = function(){
   var ctx = null;
   return {
-    get: function (id){
+    get: function(id){
       ctx = document.getElementById(id);
       return this;
     },
-    clear: function (){
+    clear: function(){
       if (ctx) {
         ctx.innerHTML = '';
         return this;
       }
     },
-    append: function (str){
+    append: function(str){
       ctx.innerHTML = str;
       return this;
     }
   }
 }();
 
-
 const UI_FLOWS = {
-  RECORD: function (msg){
+  RECORD: function(msg){
     UI.get('status').clear().append(msg.value);
   }
 };
 
-
-const sendExtensionMessage = function (msg, cb){
-  chrome.runtime.sendMessage(msg, function (response){
+const sendExtensionMessage = function(msg, cb){
+  chrome.runtime.sendMessage(msg, function(response){
     if (cb) {
       cb(response);
     }
   });
 };
 
+document.addEventListener('DOMContentLoaded', function(){
 
-document.addEventListener('DOMContentLoaded', function (){
-  document.getElementById('record').addEventListener('click', function (){
-    sendExtensionMessage({message: 'RECORD'}, function (response){
-      UI.get('status').clear().append(response.value);
-    })
-  });
-
-  document.getElementById('playback').addEventListener('click', function (){
-    sendExtensionMessage({message: 'PLAYBACK'}, function (response){
-      UI.get('status').clear().append(response.value);
+  document.getElementById('record').addEventListener('click', function(){
+    let narrowedResponse = MESSAGE_FLOWS.RECORD;
+    sendExtensionMessage({message: 'RECORD'}, function(response){
+      if (response.value in narrowedResponse) {
+        UI.get('status').clear().append(narrowedResponse[response.value]);
+      } else {
+        throw new Error({message: 'Cannot find adequate response for ' + response.value});
+      }
     });
   });
 
-  document.getElementById('stop').addEventListener('click', function (){
-    sendExtensionMessage({message: 'STOP'}, function (response){
-      UI.get('status').clear().append(response.value);
+  document.getElementById('playback').addEventListener('click', function(){
+    let narrowedResponse = MESSAGE_FLOWS.PLAYBACK;
+    sendExtensionMessage({message: 'PLAYBACK'}, function(response){
+      if (response.value in narrowedResponse) {
+        UI.get('status').clear().append(narrowedResponse[response.value]);
+      } else {
+        throw new Error({message: 'Cannot find adequate response for ' + response.value});
+      }
     });
   });
 
-  sendExtensionMessage({message: 'STATE_REQUEST'}, function (response){
-    UI.get('status').clear().append(response.value);
-  })
+  document.getElementById('stop').addEventListener('click', function(){
+    let narrowedResponse = MESSAGE_FLOWS.RECORD;
+    sendExtensionMessage({message: 'STOP'}, function(response){
+      if (response.value in narrowedResponse) {
+        UI.get('status').clear().append(narrowedResponse[response.value]);
+      } else {
+        throw new Error({message: 'Cannot find adequate response for ' + response.value});
+      }
+    });
+  });
+
+  sendExtensionMessage({message: 'STATE_REQUEST'}, function(response){
+    let narrowedResponse = MESSAGE_FLOWS.RECORD;
+    if (response.value in narrowedResponse) {
+      UI.get('status').clear().append(narrowedResponse[response.value]);
+    } else {
+      throw new Error({message: 'Cannot find adequate response for' + response.value});
+    }
+  });
 
 });
 
