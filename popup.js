de6726b11bc926e4764f1b2883f24ff401d69706
Taylor Lehman <taylorlehman111@gmail.com>
2016-09-07 23:23:21 -0700

    passes linter

--- popup.js
+++ popup.js
@@ -1,97 +1,83 @@
 const MESSAGE_FLOWS = {
-  'RECORD': {
-    'ACCEPT': 'Recording Started!',
-    'IN_PROGRESS': 'Recording already in progress. Please start a new recording',
-    'DEFAULT': 'Click record to start a new recording'
+  RECORD: {
+    ACCEPT: 'Recording Started!',
+    IN_PROGRESS: 'Recording already in progress. Please start a new recording',
+    DEFAULT: 'Click record to start a new recording'
   },
-  'PLAYBACK': {
-    'RUNNING': 'Playback Started',
-    'IN_PROGRESS': 'Playback already in progress'
+  PLAYBACK: {
+    RUNNING: 'Playback Started',
+    IN_PROGRESS: 'Playback already in progress'
   }
 };
 
-const UI = function(){
-  var ctx = null;
+const UI = (function() {
+  let ctx;
   return {
-    get: function(id){
+    get(id) {
       ctx = document.getElementById(id);
       return this;
     },
-    clear: function(){
+    clear() {
       if (ctx) {
         ctx.innerHTML = '';
-        return this;
       }
+      return this;
     },
-    append: function(str){
+    append(str) {
       ctx.innerHTML = str;
       return this;
     }
-  }
-}();
+  };
+}());
 
-const UI_FLOWS = {
-  RECORD: function(msg){
-    UI.get('status').clear().append(msg.value);
-  }
-};
-
-const sendExtensionMessage = function(msg, cb){
-  chrome.runtime.sendMessage(msg, function(response){
+const sendExtensionMessage = (msg, cb) => {
+  chrome.runtime.sendMessage(msg, response => {
     if (cb) {
       cb(response);
     }
   });
 };
 
-document.addEventListener('DOMContentLoaded', function(){
-
-  document.getElementById('record').addEventListener('click', function(){
-    let narrowedResponse = MESSAGE_FLOWS.RECORD;
-    sendExtensionMessage({message: 'RECORD'}, function(response){
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('record').addEventListener('click', () => {
+    const narrowedResponse = MESSAGE_FLOWS.RECORD;
+    sendExtensionMessage({ message: 'RECORD' }, response => {
       if (response.value in narrowedResponse) {
         UI.get('status').clear().append(narrowedResponse[response.value]);
       } else {
-        throw new Error({message: 'Cannot find adequate response for ' + response.value});
+        throw new Error({ message: `Cannot find adequate response for ${response.value}` });
       }
     });
   });
 
-  document.getElementById('playback').addEventListener('click', function(){
-    let narrowedResponse = MESSAGE_FLOWS.PLAYBACK;
-    sendExtensionMessage({message: 'PLAYBACK'}, function(response){
+  document.getElementById('playback').addEventListener('click', () => {
+    const narrowedResponse = MESSAGE_FLOWS.PLAYBACK;
+    sendExtensionMessage({ message: 'PLAYBACK' }, response => {
       if (response.value in narrowedResponse) {
         UI.get('status').clear().append(narrowedResponse[response.value]);
       } else {
-        throw new Error({message: 'Cannot find adequate response for ' + response.value});
+        throw new Error({ message: `Cannot find adequate response for ${response.value}` });
       }
     });
   });
 
-  document.getElementById('stop').addEventListener('click', function(){
-    let narrowedResponse = MESSAGE_FLOWS.RECORD;
-    sendExtensionMessage({message: 'STOP'}, function(response){
+  document.getElementById('stop').addEventListener('click', () => {
+    const narrowedResponse = MESSAGE_FLOWS.RECORD;
+    sendExtensionMessage({ message: 'STOP' }, response => {
       if (response.value in narrowedResponse) {
         UI.get('status').clear().append(narrowedResponse[response.value]);
       } else {
-        throw new Error({message: 'Cannot find adequate response for ' + response.value});
+        throw new Error({ message: `Cannot find adequate response for ${response.value}` });
       }
     });
   });
 
-  sendExtensionMessage({message: 'STATE_REQUEST'}, function(response){
-    let narrowedResponse = MESSAGE_FLOWS.RECORD;
+  sendExtensionMessage({ message: 'STATE_REQUEST' }, response => {
+    const narrowedResponse = MESSAGE_FLOWS.RECORD;
     if (response.value in narrowedResponse) {
       UI.get('status').clear().append(narrowedResponse[response.value]);
     } else {
-      throw new Error({message: 'Cannot find adequate response for' + response.value});
+      throw new Error({ message: `Cannot find adequate response for ${response.value}` });
     }
   });
-
 });
-
-
-
-
-
-
